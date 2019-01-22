2f3b8c9240ea9fb009bc9ad97545857b5e8edc98
yun <>
2019-01-22 14:13:50 +0800

    1.0.0

--- YunQiniuHelper/YunQiniuHelper/YunQiniuImgHelper.h
+++ YunQiniuHelper/YunQiniuHelper/YunQiniuImgHelper.h
@@ -10,20 +10,24 @@
 
 + (YunQiniuImgHelper *)instance;
 
-- (void)setDelegate:(id)tg;
+- (void)setDelegate:(id)tg
+__deprecated_msg("已过期, 请使用YunQiniuUploadHelper");
 
 // item =image/data(image)
 - (void)uploadImages:(NSArray *)imgList
             progress:(void (^)(CGFloat))progress
              success:(void (^)(NSArray<NSString *> *urlList))success
-             failure:(void (^)(NSError *err))failure;
+             failure:(void (^)(NSError *err))failure
+__deprecated_msg("已过期, 请使用YunQiniuUploadHelper");
 
 // item =image/data(image)
 - (void)uploadImages:(NSArray *)imgList
              success:(void (^)(NSArray<NSString *> *urlList))success
-             failure:(void (^)(NSError *err))failure;
+             failure:(void (^)(NSError *err))failure
+__deprecated_msg("已过期, 请使用YunQiniuUploadHelper");
 
 // 需要实现
-- (void)getQnPara:(getQnParaBlock)rst;
+- (void)getQnPara:(getQnParaBlock)rst
+__deprecated_msg("已过期, 请使用YunQiniuUploadHelper");
 
 @end
